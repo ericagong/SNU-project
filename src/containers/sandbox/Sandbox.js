f68b786ed0aa189175b1807217c6f1d3830eb9b8
user.email <bsbs8645@snu.ac.kr>
2021-12-09 12:32:36 +0900

    simulation check alive function

--- src/containers/sandbox/Sandbox.js
+++ src/containers/sandbox/Sandbox.js
@@ -23,87 +23,118 @@ class Sandbox extends Component {
     Suggesters = []
  
     setEnviroment = () =>{
-        let population = [4,1,1,1]
-        let userDeposit = 80
-        let suggesterDeposit = 20
-        this.RiskTakers =[]
-        this.Cowards = []
-        this.Users = []
-     
-        this.Easygoings = []
-        this.Tactfuls =[]
-        this.Suggesters = []
-        for( let r = population[0]; r>0; r--){
-          this.RiskTakers.push([0,7,userDeposit,0,0,0,'REWARD',[]])
-        }
-        for( let c = population[1]; c>0; c--){
-          this.Cowards.push([1,0,userDeposit,0,0,0,'REWARD',[]])
-        }
-        this.Users.push(this.RiskTakers)
-        this.Users.push(this.Cowards)
-        this.Users = this.Users.flat(1)
-        for( let e = population[2]; e>0; e--){
-          this.Easygoings.push([0,4.0,suggesterDeposit,4.0,0])
-        }
-        for( let t = population[3]; t>0; t--){
-          this.Tactfuls.push([0,4.0,suggesterDeposit,4.0,0])
-        }
-        this.Suggesters.push(this.Easygoings)
-        this.Suggesters.push(this.Tactfuls)
-        this.Suggesters = this.Suggesters.flat(1)
-        this.setState({round:0})
-    
+      let population = [3,2,1,1]
+      let userDeposit = 80
+      let suggesterDeposit = 20
+      this.RiskTakers =[]
+      this.Cowards = []
+      this.Users = []
+   
+      this.Easygoings = []
+      this.Tactfuls =[]
+      this.Suggesters = []
+      for( let r = population[0]; r>0; r--){
+        this.RiskTakers.push([0,7,userDeposit,0,0,0, true ,[]])
       }
-    
- 
-    match = ()=>{
-     this.RiskTakers.map((user)=>{
- //      if(user[7].length < this.Suggesters.length){
-       let candidates = []
-       if(user[3]>=3 || user[7].length==0 || (user[7].length>0 && this.Suggesters[user[7][0]][2]<0)){
-         while(candidates.length < 3){
-           let candidate = Math.floor(Math.random() * (this.Suggesters.length))
-           if(this.Suggesters[candidate][2]>0 &&(user[7][user[7].length-1]!=candidate)){
-             candidates.push(candidate);
-           }
-         }
-         user[3]=0
-       }
-       else{
-         candidates.push(user[7][0])
-       }
-       user[7] = [candidates[0], user[7]]
-       user[7]=user[7].flat(1)
-       if(this.Suggesters[candidates[0]][1]>=7){
-         this.Suggesters[candidates[0]][2] = this.Suggesters[candidates[0]][2] + 2
-         user[2] = user[2] - 2
-       }
-       else{
-         this.Suggesters[candidates[0]][2] = this.Suggesters[candidates[0]][2] - 4
-         user[2] = user[2] - 4
-         user[3] = user[3]+1
-         if(candidates[0]>this.Easygoings.length-1){
-           this.Suggesters[candidates[0]][1] = this.Suggesters[candidates[0]][1] +1
-         }
-       }
- //    }
-     })
-     this.Cowards.map((user)=>{
-       let candidates = []
-       while(candidates.length < 3){
-         let candidate = Math.floor(Math.random() * (this.Suggesters.length))
-         if(this.Suggesters[candidate][2]>0){
-           candidates.push(candidate);
-         }
-       }
-       
-       user[7] = [candidates[0], user[7]]
-       user[7].flat(1)
-       this.Suggesters[candidates[0]][2] = this.Suggesters[candidates[0]][2] + 2
-       user[2] = user[2] - 2
-     })
-     this.setState({round:++this.state.round})
-     }
+      for( let c = population[1]; c>0; c--){
+        this.Cowards.push([1,0,userDeposit,0,0,0, true ,[]])
+      }
+      this.Users.push(this.RiskTakers)
+      this.Users.push(this.Cowards)
+      this.Users = this.Users.flat(1)
+      for( let e = population[2]; e>0; e--){
+        this.Easygoings.push([0,4.0,suggesterDeposit,4.0,0, true])
+      }
+      for( let t = population[3]; t>0; t--){
+        this.Tactfuls.push([0,4.0,suggesterDeposit,4.0,0, true])
+      }
+      this.Suggesters.push(this.Easygoings)
+      this.Suggesters.push(this.Tactfuls)
+      this.Suggesters = this.Suggesters.flat(1)
+      this.setState({round:0})
+  
+    }
+  
+     match = ()=>{
+      this.RiskTakers.map((user)=>{
+        if(user[2]>0){
+          let candidates = []
+          if(user[3]>=3 || user[7].length==0 || (user[7].length>0 && this.Suggesters[user[7][0]][2]<0)){
+            while(candidates.length < 1){
+              let candidate = Math.floor(Math.random() * (this.Suggesters.length))
+              if(this.Suggesters[candidate][2]>0){
+                if((user[7][user[7].length-1]!=candidate))
+                  candidates.push(candidate);
+                else
+                  candidates.push(user[7][0])
+              }
+            }
+            user[3]=0
+          }
+          else{
+            candidates.push(user[7][0])
+          }
+          user[7] = [candidates[0], user[7]]
+          user[7]=user[7].flat(1)
+          if(this.Suggesters[candidates[0]][1]>=7){
+            this.Suggesters[candidates[0]][2] = this.Suggesters[candidates[0]][2] + 2
+            user[2] = user[2] - 2
+          }
+          else{
+            this.Suggesters[candidates[0]][2] = this.Suggesters[candidates[0]][2] - 4
+            if(this.Suggesters[candidates[0]][2]<0) this.Suggesters[candidates[0]][5] = false
+            user[2] = user[2] - 4
+            user[3] = user[3]+1
+            if(candidates[0]>this.Easygoings.length-1){
+              this.Suggesters[candidates[0]][1] = this.Suggesters[candidates[0]][1] +1
+            }
+          }
+      }
+      else{
+        user[6] = false
+      }
+      })
+      this.Cowards.map((user)=>{
+      if(user[2]>0){
+          let candidates = []
+          while(candidates.length < 1){
+            let candidate = Math.floor(Math.random() * (this.Suggesters.length))
+            if(this.Suggesters[candidate][2]>0){
+              candidates.push(candidate);
+            }
+          }
+          
+          user[7] = [candidates[0], user[7]]
+          user[7].flat(1)
+          this.Suggesters[candidates[0]][2] = this.Suggesters[candidates[0]][2] + 2
+          user[2] = user[2] - 2
+        }
+      else{
+        user[6] = false
+      }
+        
+      })
+      this.setState({round:++this.state.round})
+      }
+  
+    checkAlive = () =>{
+      let numOfDeadSuggester = 0
+      let numOfDeadUser = 0
+      this.Suggesters.map((suggester)=>{
+        if(suggester[5]==false)
+          numOfDeadSuggester++;
+      })
+      if(numOfDeadSuggester==this.Suggesters.length)
+        return false
+      
+      this.User.map((user)=>{
+        if(user[5]==false)
+            numOfDeadUser++;
+      })
+      if(numOfDeadUser==this.Users.length)
+          return false
+      return true
+    }
  
 
     clickStartSimulationHandler = () => {
