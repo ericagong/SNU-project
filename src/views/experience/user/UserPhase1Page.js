901ec4fb711dae8ac4f4641ca66c23001496814b
ericagong <48196721+ericagong@users.noreply.github.com>
2021-12-09 13:14:46 +0900

    Implementation of userPhase1Page

--- src/views/experience/user/UserPhase1Page.js
+++ src/views/experience/user/UserPhase1Page.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
-
+import UserPhase1 from '../../../containers/experience/user/UserPhase1';
 import Footer from '../../../containers/footer/Footer';
 
 class UserPhase1Page extends Component {
     render () {
+        let deposit = parseInt(this.props.match.params.deposit)
+        let suggesterID = parseInt(this.props.match.params.id)
         return (
             <div className = 'UserPhase1Page'>
                 UserPhase1Page
-
+                <UserPhase1 deposit={deposit} suggesterID={suggesterID}/>
                 <Footer/>
             </div>
         )
